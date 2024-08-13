06213c0a49deaff9e455c99fadf3d84ade69e628
LaurenceOMFoisy <mail@mfoisy.com>
2024-08-13 15:16:07 -0400

    mod gpt vision

--- R/gpt_vision.R
+++ R/gpt_vision.R
@@ -7,7 +7,8 @@
 #' @param image_path The path to the image file that you want to analyze.
 #' @param prompt An optional prompt to guide the analysis. The default prompt is "What is in this image?".
 #' @param max_tokens The maximum number of tokens to generate in the response. Defaults to 1000.
-#'
+#' @param model The model you chose to use. gpt4o by default.
+#' 
 #' @return Returns the textual interpretation of the image from the GPT model. If no valid response is received,
 #' returns `NULL`.
 #'
@@ -21,27 +22,24 @@
 #' @import httr
 #' @import jsonlite
 #' @import base64enc
-gpt_vision <- function(image_path, prompt = "What is in this image?", max_tokens = 1000) {
+gpt_vision <- function(image_path, prompt = "What is in this image?", max_tokens = 1000, model = "gpt4o") {
 
   library(httr)
   library(jsonlite)
   library(base64enc)
 
   api_key <- Sys.getenv("OPENAI_API_KEY")
-  
   # Read the binary content of the image and encode it to a Base64 string
   image_data <- readBin(image_path, "raw", file.info(image_path)$size)
   base64_image <- base64encode(image_data)
-  
   # Set headers for the API request
   headers <- add_headers(
     `Content-Type` = "application/json",
     `Authorization` = paste("Bearer", api_key)
   )
-  
   # Construct the payload
   payload <- list(
-    model = "gpt-4-vision-preview",
+    model = model,
     messages = list(
       list(
         role = "user",
@@ -53,16 +51,13 @@ gpt_vision <- function(image_path, prompt = "What is in this image?", max_tokens
     ),
     max_tokens = max_tokens
   )
-  
   # Make the API request
   response <- POST("https://api.openai.com/v1/chat/completions", 
                          headers, 
                          body = toJSON(payload, auto_unbox = TRUE), 
                          encode = "json")
-  
   # Parse the JSON response
   parsed_response <- content(response, "parsed")
-  
   # Extract and return the content of the first choice's message, if available
   if (!is.null(parsed_response$choices) && length(parsed_response$choices) >= 1) {
     choice <- parsed_response$choices[[1]]
@@ -70,6 +65,5 @@ gpt_vision <- function(image_path, prompt = "What is in this image?", max_tokens
       return(choice$message$content)
     }
   }
-  
   return(NULL)  # Return NULL if no valid response was received
 }
